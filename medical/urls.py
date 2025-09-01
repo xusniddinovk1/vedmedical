5edb1e8cba8510416ddd5d028a1a6feba08eb116
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-09-01 12:43:48 +0500

    initial commit

--- medical/urls.py
+++ medical/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
+    path('', views.home_page, name='home'),
     path("about/", views.about, name="about"),
     path("manufacturing/", views.manufacturing_page, name="manufacturing"),
     path("partners/", views.partners_page, name="partners"),
     path("galleries/", views.gallery_page, name="gallery"),
     path("products/", views.product_list, name="products"),
     path("product/<int:pk>/", views.product_detail, name="product_detail"),
-    path("news/", views.news_list, name="news_list"),
+    path("news/", views.news_list, name="news"),
 ]
